65c9d7bafe2ea334cfe4706873d777829ce99522
Eddie <eocampo52@gmail.com>
2025-01-30 10:47:28 -0600

    finished header of app. added editable player name button functinoality to app

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function GameBoard() {
+  return (
+    <ol id='game-board'>
+      <li>
+        <ol>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ol>
+      </li>
+      <li>
+        <ol>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ol>
+      </li>
+      <li>
+        <ol>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ol>
+      </li>
+    </ol>>
+  )
+}
+
+export default GameBoard
